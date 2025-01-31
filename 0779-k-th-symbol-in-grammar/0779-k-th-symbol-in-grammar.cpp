533f4809549c0a0360c8bd80a9e34a0de6620c36
Hetram Gugrwal <171897105+hetram1@users.noreply.github.com>
2025-01-31 16:13:03 +0530

    Time: 0 ms (100%), Space: 7.7 MB (83.44%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int kthGrammar(int n, int k) {
-        int left = 1;
-        int right =pow(2,n-1);
-        int curr=0;
-
-        for(int i=0;i<n;i++){
-            int mid = (right-left)/2 + left;
-            if(k<=mid){
-                right = mid;
+        int curr = 0;
+        
+        while (n > 1) {
+            int mid = (1 << (n - 2));
+            if (k > mid) {
+                k -= mid;
+                curr = 1 - curr;
             }
-            else{
-                left = mid+1;
-                curr=1-curr;
-            }
+            n--;
         }
+        
         return curr;
     }
 };
